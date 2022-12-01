d583396971318a70738fc21fc1d99ccc7c90047b
Evgeniia <evgarnaga@gmail.com>
2022-12-01 11:09:11 +0600

    Добавляет адаптив (franchise)

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,10 @@ import {ieFix} from './utils/ie-fix';
 import {initModals} from './modules/init-modals';
 import {cityMenu} from './modules/city-menu';
 import {navMenu} from './modules/nav-menu';
+
 import {franchiseOpeningSlider} from './modules/franchise-opening-slider';
+import {shopSlider} from './modules/shop-slider';
+import {reviewsSlider} from './modules/reviews-slider';
 
 // Utils
 // ---------------------------------
@@ -18,3 +21,5 @@ cityMenu();
 navMenu();
 
 franchiseOpeningSlider();
+shopSlider();
+reviewsSlider();
